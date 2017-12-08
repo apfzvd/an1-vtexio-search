ff7885157b3947136982c25b464bd4cb3050b21f
Ana Azevedo <ana.paula@agencian1.com.br>
2017-12-07 23:24:51 -0200

    Style upgrades

--- react/index.js
+++ react/index.js
@@ -25,8 +25,19 @@ class App extends Component {
 
     return (
       <div className="w-60 center pv4">
-        <h1 className="blue">Hey! Try looking for our products here: </h1>
-        <h2 className="blue">hint: Try Max!</h2>
+        <div className="fl w-100">
+          <div className="fl w-30 mv1">
+            <img src="//agencian1.com.br/wp-content/themes/n1/assets/images/logo.png" className="bw4" />
+          </div>
+          <div className="fl w-70">
+            <h1 className="f2 blue">Produto N1</h1>
+          </div>
+        </div>
+
+        <div className="mv1">
+          <h2 className="blue">Hey! Try looking for our products here: </h2>
+          <h3 className="blue">hint: Try Max!</h3>
+        </div>
 
         <div>
          <input className="w-100 br2 ba bw1 b--light-silver blue" type="text" onInput={this.changeBusca} />
